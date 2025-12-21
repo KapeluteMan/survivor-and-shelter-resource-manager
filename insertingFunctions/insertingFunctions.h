cc9f5d186cd578a8501f37df09d814f8bb0e1026
Tatarczyk Wojciech <wojtek.tatarczyk@wp.pl>
2025-12-21 15:03:18 +0100

    added inserting functions

---
+++ insertingFunctions/insertingFunctions.h
@@ -0,0 +1,7 @@
+#ifndef INSERTINGFUNCTIONS_H
+#define INSERTINGFUNCTIONS_H
+
+int insertInt(void);
+void insertString(char* text);
+
+#endif //INSERTINGFUNCTIONS_H
--- main.c
+++ main.c
@@ -2,10 +2,15 @@
 // Created by wojte on 17.12.2025.
 //
 #include <stdio.h>
+#include <string.h>
 
 #include "startPage/startPage.h"
+#include "insertingFunctions/insertingFunctions.h"
 
 int main() {
     printf("Hello World!\n");
     showStartPage();
+    char a[100];
+    insertString(a);
+    printf("%s\n", a);
 }
