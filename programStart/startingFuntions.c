ad5f643989ecbc8c46a4ea19826e2b5c9464fff1
Tatarczyk Wojciech <wojtek.tatarczyk@wp.pl>
2026-01-03 16:42:49 +0100

    fixed staring page files

---
+++ programStart/startingFuntions.c
@@ -0,0 +1,26 @@
+//
+// Created by wojte on 17.12.2025.
+//
+#include <stdio.h>
+#include "startingFuntions.h"
+
+void showStartPage() {
+    printf("                                                                       ");
+    printf("                                                                       ");
+    printf("                                                                       ");
+    printf("                                                                       ");
+    printf("                                                                       ");
+    printf("                                                                       ");
+    printf("                                                                       ");
+    printf("                                                                       ");
+    printf("                                                                       ");
+    printf("                                                                       ");
+    printf("                                                                       ");
+    printf("                                                                       ");
+    printf("                                                                       ");
+    printf("                                                                       ");
+    printf("                                                                       ");
+    printf("                                                                       ");
+    printf("                                                                       ");
+    printf("                                                                       1");
+}
