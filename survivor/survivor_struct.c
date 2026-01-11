8955c9e42cf3b618b0cb08baebcedb863f6d4d80
Tatarczyk Wojciech <wojtek.tatarczyk@wp.pl>
2026-01-11 22:49:51 +0100

    missing survivor struct

---
+++ survivor/survivor_struct.c
@@ -0,0 +1,17 @@
+#include <string.h>
+#include <stdio.h>
+
+#include "survivor_struct.h"
+
+
+void copy_survivor(const struct Survivor* from, struct Survivor* to) {
+    to->id = from ->id;
+    strcpy(to->name,from->name);
+    to->skill = from ->skill;
+    to->rations = from ->rations;
+    to->health = from ->health;
+    to->state_of_health = from ->state_of_health;
+    to->threat_level = from -> state_of_health;
+    to-> status_of_survivor = from ->status_of_survivor;
+    to->next=NULL;
+}
