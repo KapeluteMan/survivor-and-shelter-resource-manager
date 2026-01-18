0b14145f78e6e656ea399dfc663a66f4b6473521
Tatarczyk Wojciech <wojtek.tatarczyk@wp.pl>
2026-01-18 21:16:23 +0100

    add convert from int to enum for survivor

--- survivor/survivor_struct.c
+++ survivor/survivor_struct.c
@@ -15,3 +15,21 @@ void copy_survivor(const struct Survivor* from, struct Survivor* to) {
     to-> status_of_survivor = from ->status_of_survivor;
     to->next=NULL;
 }
+
+enum specialist_skill form_int_to_enum_skill(int a) {
+    switch (a) {
+        case 0: return MEDIC;
+        case 1: return ENGINEER;
+        case 2: return HUNTER;
+        default: return ORDINARY;
+    }
+}
+
+enum state_of_health form_int_to_enum_state_health(int a) {
+    switch (a) {
+        case 0: return HEALTHY;
+        case 1: return WEAKEN;
+        case 2: return SICK;
+        default: return DYING;
+    }
+}
