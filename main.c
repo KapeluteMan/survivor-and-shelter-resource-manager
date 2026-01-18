000ef7cdf9538bf6e0a1d40ca46d640ae81eaec3
AngryBrr <pawel.brejnak2005@gmail.com>
2026-01-18 20:15:41 +0100

    Dodaj funkcjonalność menu: naprawianie menu

--- main.c
+++ main.c
@@ -38,82 +38,9 @@ int main() {
     add_all_to_file(head);
     survivor_health_status_change(head);
     while (menu == 1) {
-        int option = 0;
-        int type = 0;
-        // add_one_to_file(head);
-        printf(
-            "Select an option:\n"
-            " 1 - Add the survivor\n"
-            " 2 - Check the amount of survivors \n"
-            " 3 - List the survivors\n"
-            " 4 - Delete a survivor via id \n"
-            " 5 - Segregate \n"
-            " 6 - Show Quests \n"
-            " 7 - Exit\n"
+        void survivor_menu(struct Survivor **head);
 
-            "\n");
-        int wybor1 = insert_int();
-        switch (wybor1) {
-            case 1:
-                system("cls");
-                show_title();
-                printf("Add the survivor:\n");
-                head = add_survivor(head);
-                break;
-            case 2:
-                system("cls");
-                show_title();
-                printf("Amount of survivors: %d\n", check_amount(head));
-                break;
-            case 3:
-                system("cls");
-                show_title();
-                printf("List:\n");
-                print_list(head);
-                head = delete_by_skill(head, MEDIC);
-                break;
-            case 4:
-                system("cls");
-                show_title();
-                printf("Delete a survivor:\n");
-                printf("Available survivors (who are not on a mission):\n");
-                short_print_list(head);
-                int enter_id = insert_int();
-                head = delete_by_id(head, enter_id);
-                break;
-            case 5:
-                system("cls");
-                show_title();
-                printf("Choose an option: \n"
-                       "0 - Sort by name\n"
-                       "1 - Sort by skill\n"
-                       "2 - Sort by rations\n"
-                       "3 - Sort by health\n"
-                       "4 - Sort bt threatLevel\n");
-                option = check_interval(0,4);
-                printf("\n0 - Ascending >\n1 - Descending <\n");
-                type = check_interval(0,1);
 
-                bubble_segregate(head, option, type);
-                break;
-
-            case 6:
-                system("cls");
-                show_title();
-                printf("List:\n");
-                print_quest_list(q_head);
-
-                Quest* tym = random_quest(q_head);
-                print_quest(tym);
-                free(tym);
-                break;
-            case 7:
-                menu = 0;
-                break;
-
-            default:
-                printf("Wrong option\n");
-        }
     }
 
 
