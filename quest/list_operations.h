c9b2ea43dfd17404660c41b9ddb37d2710625cc2
Tatarczyk Wojciech <wojtek.tatarczyk@wp.pl>
2026-01-19 11:09:45 +0100

    add number of survivors/quest on start validation

--- quest/list_operations.h
+++ quest/list_operations.h
@@ -6,5 +6,6 @@ struct Quest *add_quest(struct Quest *head, struct Quest *toAdd);
 struct Quest* copy_quest(struct Quest* from);
 struct Quest* random_quest(struct Quest* head);
 struct Quest *checked_finished_quest(struct Quest *head, struct Survivor *s_head, int *rations);
+int check_amount_quest(struct Quest *head);
 
 #endif //QUEST_LIST_OPERATIONS_H
